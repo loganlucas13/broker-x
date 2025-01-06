5c6d4b7e9b8e6cad039be24753b51b94d628db88
Logan Lucas <logs10658@gmail.com>
2025-01-06 17:17:07 -0600

    fixed indentation on template files

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,17 @@ import SearchBar from './components/SearchBar';
 import StockList from './components/StockList';
 
 function App() {
-  return (
-    <div className="text-center bg-stone-100 min-h-screen w-full">
-      <Header></Header>
-      <div className="pt-36 pb-12 flex justify-center">
-        <SearchBar></SearchBar>
-      </div>
-      <div className="flex justify-center">
-        <StockList></StockList>
-      </div>
-    </div>
-  );
+    return (
+        <div className="text-center bg-stone-100 min-h-screen w-full">
+            <Header></Header>
+            <div className="pt-24 pb-12 flex justify-center">
+                <SearchBar></SearchBar>
+            </div>
+            <div className="flex justify-center">
+                <StockList></StockList>
+            </div>
+        </div>
+    );
 }
 
 export default App;
